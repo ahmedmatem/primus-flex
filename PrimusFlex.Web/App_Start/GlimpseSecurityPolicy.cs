541def020787ac038893300293e5b9b9d15049c1
ahmed <ahmedmatem@gmail.com>
2016-05-17 15:40:54 +0100

    Set Glimpse policy

--- PrimusFlex.Web/App_Start/GlimpseSecurityPolicy.cs
+++ PrimusFlex.Web/App_Start/GlimpseSecurityPolicy.cs
@@ -12,11 +12,11 @@ namespace PrimusFlex.Web
         {
             // You can perform a check like the one below to control Glimpse's permissions within your application.
 			// More information about RuntimePolicies can be found at http://getglimpse.com/Help/Custom-Runtime-Policy
-			// var httpContext = policyContext.GetHttpContext();
-            // if (!httpContext.User.IsInRole("Administrator"))
-			// {
-            //     return RuntimePolicy.Off;
-			// }
+			 var httpContext = policyContext.GetHttpContext();
+             if (!httpContext.User.IsInRole("Administrator"))
+			 {
+                 return RuntimePolicy.Off;
+			 }
 
             return RuntimePolicy.On;
         }
