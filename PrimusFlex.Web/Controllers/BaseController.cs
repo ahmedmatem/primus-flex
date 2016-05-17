b8822b56a2b8a813ed970d3279cef790f0b41421
ahmed <ahmedmatem@gmail.com>
2016-05-17 18:02:34 +0100

    Set web layout page and some partial navigation views

---
+++ PrimusFlex.Web/Controllers/BaseController.cs
@@ -0,0 +1,35 @@
+﻿namespace PrimusFlex.Web.Controllers
+{
+    using System.Web.Mvc;
+    using System.Web;
+
+    using Microsoft.AspNet.Identity.Owin;
+
+    using AutoMapper;
+    using Infrastructure.Mapping;
+
+    public abstract class BaseController : Controller
+    {
+        protected ApplicationUserManager userManager;
+
+        protected IMapper Mapper
+        {
+            get
+            {
+                return AutoMapperConfig.Configuration.CreateMapper();
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                userManager = value;
+            }
+        }
+    }
+}
