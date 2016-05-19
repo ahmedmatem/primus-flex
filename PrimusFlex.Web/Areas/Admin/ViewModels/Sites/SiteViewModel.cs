f7fe28253de996b0fa3f9e452058b8a5b030e5bb
ahmed <ahmedmatem@gmail.com>
2016-05-19 23:05:08 +0100

    Start Site controller realization

---
+++ PrimusFlex.Web/Areas/Admin/ViewModels/Sites/SiteViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace PrimusFlex.Web.Areas.Admin.ViewModels.Sites
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Data.Models;
+    using Infrastructure.Mapping;
+    using Data.Models.Types;
+
+    public class SiteViewModel : IMapFrom<Site>
+    {
+        public int Name { get; set; }
+
+        public string PostCode { get; set; }
+
+        public string Address { get; set; }
+
+        public KitchenName KitchenName { get; set; }
+        
+        public bool Confirmed { get; set; }
+    }
+}
---
+++ data/PrimusFlex.Data.Models/Site.cs
@@ -0,0 +1,21 @@
+﻿namespace PrimusFlex.Data.Models
+{
+    using Common.Models;
+    using Types;
+
+    public class Site : BaseModel<int>
+    {
+        public int Name { get; set; }
+
+        public string PostCode { get; set; }
+
+        public string Address { get; set; }
+
+        public KitchenName KitchenName { get; set; }
+
+        /* Property is must be confirmed (set true) by member of admin
+        Othe users can create Site but it must be confirmed by member of admin
+        */
+        public bool Confirmed { get; set; }
+    }
+}
